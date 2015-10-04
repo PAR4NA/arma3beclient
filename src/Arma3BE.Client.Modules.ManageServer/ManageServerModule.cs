227ede778dc17f0f2756e61f210560d36ee0f6b6
Artem Muradov <tym32167@gmail.com>
2015-10-04 14:10:02 +0500

    added manage server module

---
+++ src/Arma3BE.Client.Modules.ManageServer/ManageServerModule.cs
@@ -0,0 +1,21 @@
+﻿using Arma3BE.Client.Modules.ManageServer.Views;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Arma3BE.Client.Modules.ManageServer
+{
+    public class ManageServerModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ManageServerModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("PlayersRegion", typeof(ManageServerView));
+        }
+    }
+}
---
+++ src/Arma3BE.Client.Modules.ManageServer/Views/ManageServerView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Arma3BE.Client.Modules.ManageServer.Views
+{
+    /// <summary>
+    ///     Interaction logic for ManageServerView.xaml
+    /// </summary>
+    public partial class ManageServerView : UserControl
+    {
+        public ManageServerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
